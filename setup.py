b300334e727811a7ea46d79d7aeb09289d1a2b70
pascalpas <pascalpas@me.com>
2022-08-10 09:44:56 +0200

    Renamed plugin

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points='''
         [ckan.plugins]
-        dia_theme=ckanext.dia_theme.plugin:Dia_ThemePlugin
+        cd2_theme=ckanext.dia_theme.plugin:Dia_ThemePlugin
         [babel.extractors]
         ckan = ckan.lib.extract:extract_ckan
     ''',
