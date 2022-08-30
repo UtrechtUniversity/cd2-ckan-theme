882489b5f61095d7283f5c16b6bfc213921c9fb2
pascalpas <pascalpas@me.com>
2022-08-30 08:32:53 +0200

    handy script

--- reload.sh
+++ reload.sh
@@ -4,7 +4,7 @@ sudo git pull &&
 # SOLR: search engine customization
 cp ckanext/ckan_config/schema.xml /usr/lib/ckan/default/src/ckan/ckan/config/solr/schema.xml &&
 
-# ckanext-msl-ckan plugin: custom facets
+# ckanext-msl-ckan plugin: custom facets & repeating fields
 cp ckanext/ckan_config/facets.json /usr/lib/ckan/default/src/ckanext-msl-ckan/ckanext/msl_ckan/config/facets.json &&
 cp ckanext/ckan_config/msl_index_fields.json /usr/lib/ckan/default/src/ckanext-msl-ckan/ckanext/msl_ckan/config/msl_index_fields.json &&
 
