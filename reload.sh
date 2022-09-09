ffc6de462755bf3005a61d3894a1db40d8c0a8a8
pascalpas <pascalpas@me.com>
2022-09-09 14:34:25 +0200

    reload path typo

--- reload.sh
+++ reload.sh
@@ -12,7 +12,7 @@ cp ckanext/ckan_config/facets.json /usr/lib/ckan/default/src/ckanext-msl-ckan/ck
 cp ckanext/ckan_config/msl_index_fields.json /usr/lib/ckan/default/src/ckanext-msl-ckan/ckanext/msl_ckan/config/msl_index_fields.json &&
 
 # CKAN: schema definitions
-cp -r ckanext/ckan_config/schemas /usr/lib/ckan/default/src/ckanext-msl-ckan/ckanext/msl_ckan/schemas
+cp -r ckanext/ckan_config/schemas /usr/lib/ckan/default/src/ckanext-msl-ckan/ckanext/msl_ckan/
 
 # CD2: compile css styles
 lessc ckanext/cd2_theme/less/main.less ckanext/cd2_theme/fanstatic/cd2_custom.css &&
