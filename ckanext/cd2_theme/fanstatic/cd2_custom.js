8c341ef0ca72a42b20dd745e156e9e9d39d2fdc5
pascalpas <pascalpas@me.com>
2022-09-15 09:35:51 +0200

    removed function

--- ckanext/cd2_theme/fanstatic/cd2_custom.js
+++ ckanext/cd2_theme/fanstatic/cd2_custom.js
@@ -110,24 +110,3 @@ function removeRange(currentQuery,facetName) {
     document.getElementById('searchbox').value = newQuery;
     document.getElementById('dataset-search-form').submit();
 }
-
-
-/**  
- * Fade effect on list items
- * used on snippets/package_item.html
- */
-$(window).on("load",function() {
-    $(window).scroll(function() {
-      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-      $(".fade").each(function() {
-        /* Check the location of each desired element */
-        var objectBottom = $(this).offset().top + $(this).outerHeight()
-        /* If the element is completely within bounds of the window, fade it in */
-        if (objectBottom < windowBottom) { // object comes into view (scrolling down)
-          if ($(this).css("opacity")==0) {$(this).fadeTo(300,1);}
-        } else { // object goes out of view (scrolling up)
-          if ($(this).css("opacity")==1) {$(this).fadeTo(300,0);}
-        }
-      });
-    }).scroll(); //invoke scroll-handler on page-load
-  });
