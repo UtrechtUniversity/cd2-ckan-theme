7432d26632b5f04d71ccc5b28383773b655327b4
pascalpas <pascalpas@me.com>
2023-01-16 14:38:44 +0100

    modified reload to include SOLR changes

--- reload.sh
+++ reload.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 git pull origin main &&
 
+# SOLR: search engine customization
+cp /usr/lib/ckan/default/src/ckanext-msl-ckan/ckanext/msl_ckan/config/schema.xml /usr/lib/ckan/default/src/ckan/ckan/config/solr/schema.xml &&
+
 # CD2: compile css styles
 lessc ckanext/cd2_theme/less/main.less ckanext/cd2_theme/fanstatic/cd2_custom.css &&
 
