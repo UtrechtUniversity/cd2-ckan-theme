0bc4a71a26f79bb49634b5a020927640e997aba1
pascalpas <pascalpas@me.com>
2023-01-16 14:15:07 +0100

    removed config files; now located at cd2-config repo

--- reload.sh
+++ reload.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 git pull origin main &&
 
-# CKAN: ckan.ini
-# cp ckanext/ckan_config/ckan.ini /etc/ckan/default/ckan.ini &&
-
-# SOLR: search engine customization
-cp ckanext/ckan_config/schema.xml /usr/lib/ckan/default/src/ckan/ckan/config/solr/schema.xml &&
-
-# CKAN: ckanext-msl-ckan plugin: custom facets & repeating fields flattening of SOLR indexing
-cp ckanext/ckan_config/facets.json /usr/lib/ckan/default/src/ckanext-msl-ckan/ckanext/msl_ckan/config/facets.json &&
-cp ckanext/ckan_config/msl_index_fields.json /usr/lib/ckan/default/src/ckanext-msl-ckan/ckanext/msl_ckan/config/msl_index_fields.json &&
-
-# CKAN: schema definitions
-cp -r ckanext/ckan_config/schemas /usr/lib/ckan/default/src/ckanext-msl-ckan/ckanext/msl_ckan/
-
 # CD2: compile css styles
 lessc ckanext/cd2_theme/less/main.less ckanext/cd2_theme/fanstatic/cd2_custom.css &&
 
