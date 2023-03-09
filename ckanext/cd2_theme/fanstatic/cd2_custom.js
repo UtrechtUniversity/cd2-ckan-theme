94d66c633ae258713021465ba5770f0253c1f4b7
pascalpas <pascalpas@me.com>
2023-03-09 09:44:50 +0100

    capitalize search terms

--- ckanext/cd2_theme/fanstatic/cd2_custom.js
+++ ckanext/cd2_theme/fanstatic/cd2_custom.js
@@ -11,9 +11,7 @@ if (input_q) { input_q.addEventListener('keyup', replaceCharacters); }
 function replaceCharacters() {
     var value_q = input_q.value;
     var replaced_q = value_q.replace('AND ', '&');
-    var replaced_q = replaced_q.replace('and ', '& ');
     var replaced_q = replaced_q.replace('NOT ', '-');
-    var replaced_q = replaced_q.replace('not ', '- ');
     document.getElementById('searchbox').value = replaced_q;
 };
 
