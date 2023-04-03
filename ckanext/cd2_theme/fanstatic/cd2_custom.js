7be30c083e7c67809e11cf9ce14829227e1dfb4f
pascalpas <pascalpas@me.com>
2023-04-03 14:26:52 +0200

    width of single measurements increased

--- ckanext/cd2_theme/fanstatic/cd2_custom.js
+++ ckanext/cd2_theme/fanstatic/cd2_custom.js
@@ -326,7 +326,7 @@ function waveTimeline(timepoints,barID) {
 
       barSection.style.backgroundColor = '#444';
       barSection.className = 'bar-section highLight';
-      if (section == 0) {
+      if (section == 0 || timepoints.length() == 1) {
         barSection.style.width = '5px';
       }
     }
